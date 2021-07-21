08d17bdaee719ccb3eaca4ef449b6c02b82def40
sachindusahan <sachindusahan222@gmail.com>
2021-07-21 08:59:07 +0530

    drwxrwxr-x

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-import random 
-def fun(arg):
-    print(arg)
+def demo(
+        r1c1, r1c2, r1c3,
+        r2c1, r2c2, r2c3,
+        r3c1, r3c2, r3c3
+    ):
+    all_list = [r1c1, r1c2, r1c3, r2c1, r2c2, r2c3, r3c1, r3c2, r3c3]
+    demo1 = [r1c1, "|", r1c2, "|", r1c3]
+    for i in range(3):
+        for j in demo1:
+            print(j, end="")
+        print()
+        if i != 2:
+            print("------")
+demo()
