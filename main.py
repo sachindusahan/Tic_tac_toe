c0b16798327691f0c87fc81e4a2763101ccd4ea3
sachindusahan <sachindusahan222@gmail.com>
2021-07-21 08:59:07 +0530

    drwxrwxr-x

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-import random 
-def fun(arg):
-    print(arg)
+def demo():
+    demo1 = [" ", "|", "  ", "|", " "]
+    for i in range(3):
+        for j in demo1:
+            print(j, end="")
+        print()
+        if i != 2:
+            print("------")
+demo()
