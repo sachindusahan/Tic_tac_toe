85747fe92651d9b829567b742c33e39cb1d585b9
sachindusahan <sachindusahan222@gmail.com>
2021-07-23 16:12:03 +0530

    remove play file source

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from play import user
+from play import checking
 from os import system
 # demo function
-
 
 def demo(
     r1c1, r1c2, r1c3,
@@ -42,17 +41,23 @@ while condition:
         user_input = "X"
         com = "O"
         # run code ->
+        selected = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         while True:
-            system("clear")
+            #system("clear")
+
             demo(checks[0], checks[1], checks[2], checks[3],
                  checks[4], checks[5], checks[6], checks[7], checks[8])
-            print("enter your index > 1 > n")
+            
+            for num in selected:
+                print(num, end=" > ")
             position = int(input("index: > "))
 
-            if 8 >= position >= 0:
-                checks[position] = "X"
+            if position in selected:
+                selected.remove(position)
+                checks[position-1] = "X"
+                checks[checking(checks, selected) - 1] = "O"
 
-                # < -
+        # < -
     elif user_input.lower() == "o":
         user_input = "O"
         com = "X"
--- play.py
+++ play.py
@@ -1,9 +1,17 @@
+empt = " "
+checks = [empt, empt, empt, empt, empt, empt, empt, empt, empt]
+selected = [1, 2, 3, 5, 6, 7, 9]
 # 0 1 2
 # 3 4 5
 # 6 7 8
 
-def user(arg):
-    pass
+
+def checking(arg1, arg2):
+    # arg1 = all places
+    # arg2 = reminder
+
+    index = 5
+    return index
 
 
-
+print(checking(checks, selected))
