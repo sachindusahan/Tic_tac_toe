69ed36ed421231fb78e1abc0dacc859f5138a30a
sachindusahan <sachindusahan222@gmail.com>
2021-07-30 21:58:14 +0530

    created a checking function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from play import checking
 from os import system
 # demo function
 
+
 def demo(
     r1c1, r1c2, r1c3,
     r2c1, r2c2, r2c3,
@@ -41,21 +42,34 @@ while condition:
         user_input = "X"
         com = "O"
         # run code ->
-        selected = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        not_selected = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         while True:
-            #system("clear")
+            # system("clear")
 
             demo(checks[0], checks[1], checks[2], checks[3],
                  checks[4], checks[5], checks[6], checks[7], checks[8])
-            
-            for num in selected:
-                print(num, end=" > ")
-            position = int(input("index: > "))
 
-            if position in selected:
-                selected.remove(position)
-                checks[position-1] = "X"
-                checks[checking(checks, selected) - 1] = "O"
+            for num in not_selected:
+                print(num, end=" > ")
+
+            try:
+                position = int(input("index: > "))
+
+                if position in not_selected:
+                    del not_selected[position-1]  # remove selected value
+                    checks[position-1] = "X"
+                    # code
+                    # computer value
+                    checks[checking(checks, not_selected,
+                                    user_input, com) - 1] = "O"
+                else:
+                    print("Already used")
+
+                    # end code
+            except ValueError:
+                print("Please enter a valid index!")
+            except IndexError:
+                print("Index error")
 
         # < -
     elif user_input.lower() == "o":
--- play.py
+++ play.py
@@ -1,24 +1,37 @@
 empt = " "
 checks = [empt, empt, empt, empt, empt, empt, empt, empt, empt]
-selected = [1, 2, 3, 5, 6, 7, 9]
+not_selected = [1, 2, 3, 5, 6, 7, 9]
 # 0 1 2
 # 3 4 5
 # 6 7 8
+lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
 
 
-def checking(arg1, arg2):
+def checking(arg1, arg2, arg3, arg4):
     # arg1 = all places
-    # arg2 = reminder
+    # arg2 = not_selected
+    # arg3 = user input
+    # arg4 = com
+    index = 1
+    for line in lines:
+        for i in line:
+            if arg1[i] == arg3:
+                index += 1
 
-    index = 5
-    return index
+        if index == 2:
+            return_value = line.index(empt)
+            return return_value
+        else:
+            for no in arg2:
+                return no
 
 
-print(checking(checks, selected))
-
-def func():
-    print("hello world")
-    
-    return None
-
-    print()
